5db89bcc7298482675649e0082f44bfaf7424f24
Liel <liel.izcha321@gmail.com>
2021-09-09 16:09:28 +0300

    first commit

---
+++ homePage.js
@@ -0,0 +1,15 @@
+theBigDiv.innerHTML = `
+<header>
+<nav>
+      <ul>
+      <a href=""><li>Shirts </li></a>
+            <a href=""> <li>dresses </li></a>
+            <a href=""> <li> Swimwear </li></a>
+            <a href=""> <li>pants </li></a>
+        </ul>
+        <img src="" alt="">
+     </nav>
+</header>
+<main></main>
+<footer></footer>
+`;
