40bd661548ccab2a57ac2667c9d2679921934123
Liel <liel.izcha321@gmail.com>
2021-09-09 17:44:12 +0300

    Adding files and updating an array

--- code/homePage.js
+++ code/homePage.js
@@ -4,10 +4,14 @@ theBigDiv.innerHTML = `
       <ul>
       <a href=""><li>Shirts </li></a>
             <a href=""> <li>dresses </li></a>
+            <a href=""> <li>something different </li></a>
+            <a href=""> <li>something else </li></a>
+            <a href=""> <li>something more</li></a>
+
             <a href=""> <li> Swimwear </li></a>
             <a href=""> <li>pants </li></a>
         </ul>
-        <img src="" alt="">
+        <a href=""><img src="/media/logo_size_fav.jpg" id="imgLogoHome" alt=""></a>
      </nav>
 </header>
 <main></main>
