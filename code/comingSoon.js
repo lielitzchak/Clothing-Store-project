f5c7fe9ca2f6aeab0d9562d25ec77f593d306551
Liel <liel.izcha321@gmail.com>
2021-09-11 00:16:35 +0300

    add js

--- code/comingSoon.js
+++ code/comingSoon.js
@@ -1,4 +1,21 @@
-
-
-
-<img src="media/photos/waiting coming soon.jpg" id="comingSoonImg" alt="">
+var comingSoonElement = document.getElementById("comingSoonElement");
+comingSoonElement.innerHTML = `
+<header>
+    <nav>
+         <ul>     
+            <a href=""><img src="/media/photos/logo_size_fav-removebg-preview.png" id="imgLogoHome" alt=""></a>
+            <a href=""><li>Shirts </li></a>
+            <a href=""><li>dresses </li></a>
+            <a href=""><li>Shoes</li></a>
+            <a href="#"><li>Bags</li></a>
+            <a href="#"><li>accessories</li></a>
+            <a href=""><li> Swimwear </li></a>
+            <a href=""><li>pants </li></a>
+            <a href=""><img src="/media/photos/icons8-shopping-bag-50.png" id="shoppingBag" alt=""></a>        
+         </ul>
+    </nav>
+</header>
+<main><img src="/media/photos/waiting coming soon.jpg" id="comingSoonImg" alt=""> 
+</main>
+<footer></footer>
+`;
--- code/homePage.js
+++ code/homePage.js
@@ -6,8 +6,8 @@ theBigDiv.innerHTML = `
             <a href=""><li>Shirts </li></a>
             <a href=""><li>dresses </li></a>
             <a href=""><li>Shoes</li></a>
-            <a href=""><li>Bags</li></a>
-            <a href=""><li>accessories</li></a>
+            <a href="pages/comingSoon.html"><li>Bags</li></a>
+            <a href="pages/comingSoon.html"><li>accessories</li></a>
             <a href=""><li> Swimwear </li></a>
             <a href=""><li>pants </li></a>
             <a href=""><img src="media/photos/icons8-shopping-bag-50.png" id="shoppingBag" alt=""></a>        
