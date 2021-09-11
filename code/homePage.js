b9090bf3dd12b1d294ebeb435105e5beba073c63
Liel <liel.izcha321@gmail.com>
2021-09-11 16:22:08 +0300

    Update

--- code/homePage.js
+++ code/homePage.js
@@ -1,19 +1,61 @@
 theBigDiv.innerHTML = `
 <header>
-<nav>
-      <ul>     
-            <a href=""><img src="/media/photos/logo_size_fav-removebg-preview.png" id="imgLogoHome" alt=""></a>
-            <a href=""><li>Shirts </li></a>
-            <a href=""><li>dresses </li></a>
-            <a href=""><li>Shoes</li></a>
-            <a href="pages/comingSoon.html"><li>Bags</li></a>
-            <a href="pages/comingSoon.html"><li>accessories</li></a>
-            <a href=""><li> Swimwear </li></a>
-            <a href=""><li>pants </li></a>
-            <a href=""><img src="media/photos/icons8-shopping-bag-50.png" id="shoppingBag" alt=""></a>        
-      </ul>
+<nav id="header_nav_home_page">
+<ul id="header_ul_home_page">     
+      <a href="index.html"><img src="/media/photos/logo_size_fav-removebg-preview.png" id="imgLogoHome" alt=""></a>
+      <a href="pages/shirts.html"><li class="header_li_home_page">Shirts </li></a>
+      <a href="pages/dresses.html"><li class="header_li_home_page">dresses </li></a>
+      <a href="pages/comingSoon.html"><li class="header_li_home_page">Shoes</li></a>
+      <a href="pages/comingSoon.html"><li class="header_li_home_page">Bags</li></a>
+      <a href="pages/comingSoon.html"><li class="header_li_home_page">accessories</li></a>
+      <a href="pages/Swimwear.html"><li class="header_li_home_page"> Swimwear </li></a>
+      <a href="pages/pants.html"><li class="header_li_home_page">pants </li></a>
+      <a href="pages/shoppingBag.html"><img src="media/photos/icons8-shopping-bag-50.png" id="shoppingBag" alt=""></a>        
+</ul>
 </nav>
 </header>
-<main></main>
+<main>
+<a href="" class="a_home_page">
+  <section class="section_home_page">
+    <h1 class="h1_home_page">shirt</h1>
+    <img src="/media/photos/shirtWomen1.jpg" class="img_home_page" alt="" />
+  </section>
+</a>
+<a href="" class="a_home_page">
+  <section class="section_home_page">
+    <img src="media/photos/dressestWomen1.jpg" class="img_home_page" alt="" />
+    <h1 class="h1_home_page">dresses</h1>
+  </section>
+</a>
+<a href="" class="a_home_page">
+  <section class="section_home_page">
+    <h1 class="h1_home_page">shoes</h1>
+    <img src="media/photos/shoesWomen1.jpg" class="img_home_page" alt="" />
+  </section>
+</a>
+<a href="" class="a_home_page">
+  <section class="section_home_page">
+    <img src="/media/photos/bagsWomen.jpg" class="img_home_page" alt="" />
+    <h1 class="h1_home_page">bags</h1>
+  </section>
+</a>
+<a href="" class="a_home_page">
+  <section class="section_home_page">
+    <h1 class="h1_home_page">accessories</h1>
+    <img src="/media/photos/accessorieswomen.jpg" class="img_home_page" alt="" />
+  </section>
+</a>
+<a href="" class="a_home_page">
+  <section class="section_home_page">
+    <img src="/media/photos/swimmerWomen2.jpg" class="img_home_page" alt="" />
+    <h1 class="h1_home_page">swimwear</h1>
+  </section>
+</a>
+<a href="" class="a_home_page">
+  <section class="section_home_page">
+    <h1 class="h1_home_page">pants</h1>
+    <img src="/media/photos/pantsWomen3.jpg" class="img_home_page" alt="" />
+  </section>
+</a></main>
 <footer></footer>
 `;
