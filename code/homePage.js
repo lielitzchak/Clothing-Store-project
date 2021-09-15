f28e9f0d069d0a1486cd7e70967d5d78e9e3d57c
Liel <liel.izcha321@gmail.com>
2021-09-15 14:15:47 +0300

    chang var to let

--- code/homePage.js
+++ code/homePage.js
@@ -1,5 +1,7 @@
+let mobile_container = document.getElementById("mobile_container");
+let navBarMobile = mobile_container.offsetTop;
 function clickMenu() {
-  var header_nav_home_page_for_mobile = document.getElementById(
+  let header_nav_home_page_for_mobile = document.getElementById(
     "header_nav_home_page_for_mobile"
   );
   if (header_nav_home_page_for_mobile.style.display === "block") {
