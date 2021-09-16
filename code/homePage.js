97356c0520c51ff8e52c84c62b802bbbdad71dbb
Liel <liel.izcha321@gmail.com>
2021-09-16 21:00:34 +0300

    add  js

--- code/homePage.js
+++ code/homePage.js
@@ -1,3 +1,5 @@
+// להעביר את הפונקציה הזו לטלפון , אם זה רלוונטי
+
 function clickMenu() {
   let header_nav_home_page_for_mobile = document.getElementById(
     "header_nav_home_page_for_mobile"
@@ -8,14 +10,16 @@ function clickMenu() {
     header_nav_home_page_for_mobile.style.display = "block";
   }
 }
-the_form.onSubmit = (e) => {
-  e.preventDefault();
+
+the_form.onSubmit = (event) => {
+  event.preventDefault();
   let user_email_test = document.getElementById("user_email_test");
-  Check_if_similar(user_email, user_email_test);
+
+  Check_if_similar(user_email_input, user_email_test);
 };
 
 function Check_if_similar(testOne, test_two) {
-  if (testOne == test_two) {
+  if (testOne.value === test_two.value) {
     alert("good");
   } else {
     alert("not the same");
