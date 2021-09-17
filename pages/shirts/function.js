fa814efc9e2209f43c8bf12f65a39c3bdfc50ff6
Liel <liel.izcha321@gmail.com>
2021-09-17 14:22:25 +0300

    Switching between content

--- pages/shirts/function.js
+++ pages/shirts/function.js
@@ -1,15 +1,25 @@
-for (let i = 0; i < item.length; i++) {
-  if (item[i].category == "Shirts") {
-    div_for_main.innerHTML += `
-          <div class="div_for_sections">
-            <section  class="section_category_mobile">
-                <img onclick="toggleImage(event)" data-img2="${item[i].picture[1]}" class="img_category_mobile first" src="${item[i].picture[0]}" alt="">
-              <a href="" class="add-link">
-                <p>${item[i].itemName}</p>
-                <p>$ ${item[i].price}</p>
-                <p>add/delete from shopping bag</p>
-              </a>
-            </section>
-        </div>`;
+
+
+clickMenu();
+toggleImage();
+function clickMenu() {
+  let header_nav_home_page_for_mobile = document.getElementById(
+    "header_nav_home_page_for_mobile"
+  );
+  if (header_nav_home_page_for_mobile.style.display === "block") {
+    header_nav_home_page_for_mobile.style.display = "none";
+  } else {
+    header_nav_home_page_for_mobile.style.display = "block";
   }
+}
+
+function toggleImage(evt) {
+  evt.stopPropagation();
+
+  const src1 = evt.target.getAttribute("src");
+  const dataAttrImgUrl = evt.target.getAttribute("data-img2");
+
+  evt.target.setAttribute("src", dataAttrImgUrl);
+  evt.target.setAttribute("data-img2", src1);
+  console.log({ src1, dataAttrImgUrl });
 }
