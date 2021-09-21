8f68ace5f4cbff1912b80d2c615056d1d5d790f2
Liel <liel.izcha321@gmail.com>
2021-09-22 01:01:05 +0300

    add js +html

--- pages/shirts/mainDynamic.js
+++ pages/shirts/mainDynamic.js
@@ -6,7 +6,7 @@ for (let i = 0; i < item.length; i++) {
                 <img onclick="toggleImage(event)" data-img2="${item[i].picture[1]}" class="img_category_mobile first" src="${item[i].picture[0]}" alt="">
                 <div class="add-link">
                 <p class="item_name"> <b>Item name:</b> ${item[i].itemName}</p>
-                <p class="click_to_add_to_the_cart"><b>id:</b> ${item[i].id}</p>
+                <p class="click_to_add_to_the_cart item_name"><b>id:</b> ${item[i].id}</p>
                 <p class="item_price"><b>Item price:</b> $${item[i].price}</p>
                 </div>
                 <div>
