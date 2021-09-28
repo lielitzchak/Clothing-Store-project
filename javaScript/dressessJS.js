f354c24fe1685971dc5206ef71e7f1b7d801d4cf
Liel <liel.izcha321@gmail.com>
2021-09-28 09:11:24 +0300

    shirts

--- javaScript/dressessJS.js
+++ javaScript/dressessJS.js
@@ -8,6 +8,8 @@ for (let i = 0; i < item.length; i++) {
                 <p class="item_name"> <b>Item name:</b> ${item[i].itemName}</p>
                 <p class="click_to_add_to_the_cart item_name"><b>id:</b> ${item[i].id}</p>
                 <p class="item_price"><b>Item price:</b> $${item[i].price}</p>
+                <p class="item_price"><b>Item description:</b> ${item[i].description}</p>
+
                 </div>
                 <div>
                 <a href="javascript:void(0)" id="add_${item[i].id}"> <img src="${shopping_cart_img}" class="shopping_img_cart_add add_${item[i].id}"> </a>
--- javaScript/shirtsJS.js
+++ javaScript/shirtsJS.js
@@ -11,7 +11,6 @@ function all_produces() {
                 <p class="click_to_add_to_the_cart item_name"><b>id:</b> ${item[i].id}</p>
                 <p class="item_price"><b>Item price:</b> $${item[i].price}</p>
                 <p class="item_price"><b>Item description:</b> ${item[i].description}</p>
-
                 </div>
                 <div>
                 <a onclick="add_$(${item[i].price},'${item[i].description}' ,'${item[i].category}' ,'${item[i].picture[0]}' ,'${item[i].itemName}' , ${item[i].id} , event)" href="#" id="${item[i].id}">
@@ -53,31 +52,6 @@ function toggleImage(e) {
   e.target.setAttribute("src", dataAttrImgUrl);
   e.target.setAttribute("data-img2", src1);
 }
-
-// function printToDome() {
-//   for (let i = 0; i < item.length; i++) {
-//     if (item[i].category == "Shirts") {
-//       div_for_main.innerHTML += `
-//         <div class="div_for_sections">
-//           <section  class="section_category_mobile">
-//                 <img onclick="toggleImage(event)" data-img2="${item[i].picture[1]}" class="img_category_mobile first" src="${item[i].picture[0]}" alt="">
-//                 <div class="add-link">
-//                     <p class="item_name"> <b>Item name:</b> ${item[i].itemName}</p>
-//                     <p class="click_to_add_to_the_cart item_name"><b>id:</b> ${item[i].id}</p>
-//                     <p class="item_price"><b>Item price:</b> $${item[i].price}</p>
-//                 </div>
-//                 <div>
-//                     <a onclick=" add_$(${item[i].id})" href="" id="${item[i].id}">
-//                     <img src="${shopping_cart_img}" class="shopping_img_cart_add add_${item[i].id}"> </a>
-//                     <a onclick=" remove_$(${item[i].id}}, '${item[i].itemName}', ${item[i].price}, '${item[i].category}')" href="#" id="remove_${item[i].id}"><img src="${delete_item}" class="shopping_img_cart_remove"> </a>
-//                     <p class="text_on_img">Click on the image to see more</p>
-
-//                 </div>
-//             </section>
-//         </div>`;
-//     }
-//   }
-// }
 
 
 
